dbb23b9501b53a7317c7e0c0527815a5b33607d3
Gabe Irina <irinagabe@outlook.com>
2022-07-13 04:13:19 +0300

    adding deployment & service files

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a93ff3933612c4b0e95c805027aba85b-1704265837.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
